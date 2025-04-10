53a6f3d083cbd78d5e753b98662be9b2610908e1
Pinninti Pavan <pinnintipavan04@gmail.com>
2025-04-10 17:08:28 +0530

    update layout file

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,14 +2,14 @@ import { isAuthenticated } from '@/lib/actions/auth.action'
 import { redirect } from 'next/navigation'
 import { ReactNode } from 'react'
 
-const AuthLayout = async ({children}: {children: ReactNode}) => {
-
+const AuthLayout = async ({ children }: { children: ReactNode }) => {
   const isUserAuthenticated = await isAuthenticated()
-  
-  if(!isUserAuthenticated) redirect('/')
-      
+
+  // ✅ Redirect logged-in users AWAY from auth pages
+  if (isUserAuthenticated) redirect('/')
+
   return (
-    <div className='auth-layout'>
+    <div className="auth-layout">
       {children}
     </div>
   )
